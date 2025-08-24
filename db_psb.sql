0dd2cf7f45fc80df9464c417928c36f45bfbbb0a
Muhammad Irsyad <icatecss@gmail.com>
2025-08-24 21:39:40 +0700

    ganti bang sqlnya

--- db_psb.sql
+++ db_psb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 24 Agu 2025 pada 14.58
+-- Waktu pembuatan: 24 Agu 2025 pada 16.39
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -20,6 +20,37 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_psb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `username_admin` varchar(50) DEFAULT NULL,
+  `password_admin` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `dokumen`
+--
+
+CREATE TABLE `dokumen` (
+  `id_dokumen` int(11) NOT NULL,
+  `nik` varchar(16) DEFAULT NULL,
+  `nis` int(11) DEFAULT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  `jenis_dokumen` varchar(50) DEFAULT NULL,
+  `file_path` varchar(255) DEFAULT NULL,
+  `tanggal_upload` datetime DEFAULT NULL,
+  `status_verifikasi` enum('Terverifikasi','Ditolak','Menunggu') DEFAULT NULL,
+  `catatan_admin` varchar(255) DEFAULT 'Belum ada catatan!',
+  `id_jalur` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -42,9 +73,60 @@ INSERT INTO `jalur_pendaftaran` (`id_jalur`, `jenis_jalur`, `kuota`) VALUES
 (2, 'Prestasi', NULL),
 (3, 'Afirmasi', NULL);
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pendaftar`
+--
+
+CREATE TABLE `pendaftar` (
+  `id_pendaftar` int(11) NOT NULL,
+  `nama_lengkap_ortu` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `no_hp` varchar(15) DEFAULT NULL,
+  `nik` varchar(16) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `foto_profil` varchar(50) DEFAULT 'default-profile.jpg'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `nis` int(11) NOT NULL,
+  `id_pendaftar` int(11) DEFAULT NULL,
+  `id_jalur` int(11) DEFAULT NULL,
+  `nama_lengkap_siswa` varchar(100) DEFAULT NULL,
+  `nik_siswa` varchar(16) DEFAULT NULL,
+  `nisn` varchar(20) DEFAULT NULL,
+  `asal_sekolah` varchar(100) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `tempat_lahir` varchar(50) DEFAULT NULL,
+  `tanggal_lahir` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indeks untuk tabel `dokumen`
+--
+ALTER TABLE `dokumen`
+  ADD PRIMARY KEY (`id_dokumen`),
+  ADD KEY `nis` (`nis`),
+  ADD KEY `id_admin` (`id_admin`),
+  ADD KEY `fk_dokumen_pendaftar_nik` (`nik`),
+  ADD KEY `fk_dokumen_jalur` (`id_jalur`);
 
 --
 -- Indeks untuk tabel `jalur_pendaftaran`
@@ -53,14 +135,73 @@ ALTER TABLE `jalur_pendaftaran`
   ADD PRIMARY KEY (`id_jalur`);
 
 --
+-- Indeks untuk tabel `pendaftar`
+--
+ALTER TABLE `pendaftar`
+  ADD PRIMARY KEY (`id_pendaftar`),
+  ADD UNIQUE KEY `nik` (`nik`);
+
+--
+-- Indeks untuk tabel `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`nis`),
+  ADD KEY `id_pendaftar` (`id_pendaftar`),
+  ADD KEY `id_jalur` (`id_jalur`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `dokumen`
+--
+ALTER TABLE `dokumen`
+  MODIFY `id_dokumen` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `jalur_pendaftaran`
 --
 ALTER TABLE `jalur_pendaftaran`
   MODIFY `id_jalur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `pendaftar`
+--
+ALTER TABLE `pendaftar`
+  MODIFY `id_pendaftar` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `siswa`
+--
+ALTER TABLE `siswa`
+  MODIFY `nis` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `dokumen`
+--
+ALTER TABLE `dokumen`
+  ADD CONSTRAINT `dokumen_ibfk_1` FOREIGN KEY (`nis`) REFERENCES `siswa` (`nis`) ON DELETE CASCADE,
+  ADD CONSTRAINT `dokumen_ibfk_2` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`),
+  ADD CONSTRAINT `fk_dokumen_jalur` FOREIGN KEY (`id_jalur`) REFERENCES `jalur_pendaftaran` (`id_jalur`),
+  ADD CONSTRAINT `fk_dokumen_pendaftar_nik` FOREIGN KEY (`nik`) REFERENCES `pendaftar` (`nik`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `siswa`
+--
+ALTER TABLE `siswa`
+  ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`id_pendaftar`) REFERENCES `pendaftar` (`id_pendaftar`),
+  ADD CONSTRAINT `siswa_ibfk_2` FOREIGN KEY (`id_jalur`) REFERENCES `jalur_pendaftaran` (`id_jalur`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
